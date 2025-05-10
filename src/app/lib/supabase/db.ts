2be4bd28bb1421b8cb93761eae1c1228509cdf64
Fadhil Akhmad <fadhilradhian@gmail.com>
2025-05-10 20:47:24 +0700

    init + db configs

---
+++ src/app/lib/supabase/db.ts
@@ -0,0 +1,29 @@
+import {drizzle} from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as dotenv from 'dotenv';
+import * as schema from '../../../../migrations/schema';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+
+dotenv.config({path: '.env'});
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in .env file');
+}
+
+const client = postgres(process.env.DATABASE_URL as string, {
+    max: 1,
+    idle_timeout: 5,
+});
+const db = drizzle(client, {
+    schema: schema,
+});
+const migrateDb = async () => {
+    try {
+        await migrate(db,{ migrationsFolder : "migrations" });
+        console.log('Database migrated successfully');
+    } catch (error) {
+        console.error('Error migrating database:', error);
+    }
+}
+
+migrateDb();
+export default db;
